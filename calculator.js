2695552f560bcbfdf6eb9b3a7de75305a7856247
Sara Oscarsson <oscarsson.sara@hotmail.com>
2020-10-21 17:05:47 +0200

    html,css,js

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function getSelectedValue(){
+    let selected = document.getElementById("selectValue").value
+    console.log(selected)
+}
+function calculate(){
+    let selected = document.getElementById("selectValue").value
+    let firstNumber = document.getElementById("first").value
+    let secondNumber = document.getElementById("second").value
+    let svar = document.getElementById("svar")
+    console.log(firstNumber.typeof) 
+       
+    switch(selected){
+        case "+":            
+        svar.innerText = eval(Number(firstNumber) + Number(secondNumber))
+        break
+        case "-":            
+        svar.innerText = eval(Number(firstNumber) - Number(secondNumber))
+        break
+        case "*":            
+            svar.innerText = eval(Number(firstNumber) * Number(secondNumber))
+            break
+            case "/":            
+            svar.innerText = eval(Number(firstNumber) / Number(secondNumber))
+            break            
+        }
+    }    
+    function eraseContent(){
+        let selected = document.getElementById("selectValue").value = "+"
+        
+        let firstNumber = document.getElementById("first").value = ""
+        
+        
+        let secondNumber = document.getElementById("second").value = ""
+        let svar = document.getElementById("svar").innerText = ""
+}
